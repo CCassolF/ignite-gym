3c9741213f14a8ed1e2eff777303c58f690e2eea
Carlos Cassol <carloscassolfilho@gmail.com>
2024-08-17 00:51:08 -0300

    feat: component group

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,11 +1,29 @@
-import { VStack } from '@gluestack-ui/themed'
+import { HStack, VStack } from '@gluestack-ui/themed'
+import { useState } from 'react'
 
+import { Group } from '@/components/group'
 import { HomeHeader } from '@/components/home-header'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <HStack>
+        <Group
+          name="Costas"
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+
+        <Group
+          name="Ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
   )
 }
